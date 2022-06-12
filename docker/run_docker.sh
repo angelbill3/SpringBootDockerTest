09c8cbba41c067f9c4f5c90dbc6143e26037e919
angelbill3 <angelbill3@163.com>
2022-06-12 19:17:05 +0800

    Update run_docker.sh

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,4 +1,4 @@
 mv ../target/*.jar ./
-docker rm $(docker stop $(docker ps -aq --filter="ancestor=jenkins-test"))
+/usr/local/bin/docker rm $(docker stop $(docker ps -aq --filter="ancestor=jenkins-test"))
 /usr/local/bin/docker build -t jenkins-test .
 /usr/local/bin/docker run -d -p 8081:8081 -t jenkins-test
