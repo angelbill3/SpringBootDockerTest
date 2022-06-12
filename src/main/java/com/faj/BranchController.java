ad6f0a98e17ebc701c5fad36eb53e28846d26ee9
anjing <angelbill3@163.com>
2022-06-12 17:41:19 +0800

    a

---
+++ src/main/java/com/faj/BranchController.java
@@ -0,0 +1,17 @@
+package com.faj;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Allen on 2022/6/12
+ */
+@RestController
+public class BranchController {
+
+    @GetMapping("branch")
+    public String getBranch() {
+        return "branch: jenkins-test/v1.0/JIRA-1/feature-1";
+    }
+
+}
