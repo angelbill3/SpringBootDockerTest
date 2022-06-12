044362e40bc20e0f39678df3a0a70d3d3aba92e4
anjing <angelbill3@163.com>
2022-06-12 19:14:57 +0800

    v1

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,3 +1,4 @@
 mv ../target/*.jar ./
-/usr/local/bin/docker build -t jenkens-test .
-/usr/local/bin/docker run -d -p 8081:8081 -t jenkens-test
+docker rm $(docker stop $(docker ps -aq --filter="ancestor=jenkins-test"))
+/usr/local/bin/docker build -t jenkins-test .
+/usr/local/bin/docker run -d -p 8081:8081 -t jenkins-test
